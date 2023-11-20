3164229c8687052eeb0af17236ce6356bd20d5dc
jimvid <victor@jimvid.com>
2023-11-20 14:19:40 +0100

    stuff

--- frontend/apps/react/modules/items/src/lib/items.tsx
+++ frontend/apps/react/modules/items/src/lib/items.tsx
@@ -12,7 +12,6 @@ export function Items(props: ItemsProps) {
     <StyledItems>
       <h1>Welcome to Items!</h1>
       <p>Hello there matey</p>
-      <p>Hello there matey</p>
     </StyledItems>
   );
 }
