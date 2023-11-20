2e5f008fec631516b072d16b5541c6b6a9760a63
jimvid <victor@jimvid.com>
2023-11-20 14:44:41 +0100

    change

--- frontend/apps/react/modules/items/src/lib/items.tsx
+++ frontend/apps/react/modules/items/src/lib/items.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export interface ItemsProps {}
 
 const StyledItems = styled.div`
-  color: pink;
+  color: yellow;
 `;
 
 export function Items(props: ItemsProps) {
