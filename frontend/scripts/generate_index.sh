2a7003dce045baad86697f4cbdd2ba303c148dff
jimvid <victor@jimvid.com>
2023-11-23 13:00:07 +0100

    deploy list of reports, first try

---
+++ frontend/scripts/generate_index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")/.."
+
+echo "<html><head><title>Coverage Reports</title></head><body>" > index.html
+echo "<h1>Coverage Reports</h1><ul>" >> index.html
+
+# Read the last 20 lines from report_history.txt
+tail -20 report_history.txt | while read report; do
+    echo "<li><a href='coverage_reports/$report/index.html'>$report</a></li>" >> index.html
+done
+
+echo "</ul></body></html>" >> index.html
+
+mv index.html coverage_reports/
