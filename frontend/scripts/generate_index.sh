7cc253d04c5216e3707401d659d954df36cf04d3
jimvid <victor@jimvid.com>
2023-11-23 14:10:38 +0100

    read folders not from txt

--- frontend/scripts/generate_index.sh
+++ frontend/scripts/generate_index.sh
@@ -2,12 +2,10 @@
 # Go to the root of the proect
 cd "$(dirname "$0")/../.."
 
-# Generate some html
 echo "<html><head><title>Coverage Reports</title></head><body>" > index.html
 echo "<h1>Coverage Reports</h1><ul>" >> index.html
 
-# Read the last 20 lines from report_history.txt
-tail -20 report_history.txt | while read report; do
+ls -1 coverage_reports/ | sort -r | head -20 | while read report; do
     echo "<li><a href='coverage_reports/$report/index.html'>$report</a></li>" >> index.html
 done
 
